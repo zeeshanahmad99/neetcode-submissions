7c31849c2ec4d8bf67acd46dcf4d12d19b0ddfc1
Zeeshan Ahmad <zeems1234@gmail.com>
2026-06-02 17:47:02 +0500

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.js
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head) {
+        let mid = head
+        let tail = head
+
+        while(tail.next !== null) {
+            tail = tail.next
+            if(tail.next) tail = tail.next
+
+            mid = mid.next
+        }
+
+        const midNode = mid
+
+        const stack = []
+
+        while(mid) {
+            const cur = mid
+            mid = mid.next
+            cur.next = null
+            stack.push(cur)
+        }
+
+        let insertN = true
+
+        let newHead = head
+
+
+        while(newHead.next && newHead !== midNode) {
+            if(insertN) {
+                const lastNode = stack.pop()
+                const temp = newHead.next
+                newHead.next = lastNode
+                newHead = newHead.next
+                newHead.next = temp
+
+                insertN = false
+            } else {
+                newHead = newHead.next
+                insertN = true
+            }
+        }
+
+        if(stack.length) {
+            newHead.next = stack.pop()
+            newHead = newHead.next
+        }
+        
+        newHead.next = null
+
+        return head
+    }
+}
