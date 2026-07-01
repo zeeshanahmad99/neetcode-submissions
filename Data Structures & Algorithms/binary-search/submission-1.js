9853c7b1a226cc469720611952fd49d5c062f650
Zeeshan Ahmad <zeems1234@gmail.com>
2026-07-01 12:04:04 +0500

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0
+        let right = nums.length - 1
+
+        while(left <= right) {
+            const mid = Math.floor((left + right) / 2)
+
+            if(nums[mid] === target) return mid
+
+            if(target > nums[mid]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+
+        return -1
+    }
+}
