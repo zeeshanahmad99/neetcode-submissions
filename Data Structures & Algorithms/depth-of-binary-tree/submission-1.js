fbc8894015e36a98af613086bb6c2d4e58b28c46
Zeeshan Ahmad <zeems1234@gmail.com>
2026-07-01 22:59:44 +0500

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+        if(!root) return 0
+
+        const left = this.maxDepth(root.left)
+        const right = this.maxDepth(root.right)
+
+        return Math.max(left, right) + 1
+    }
+}
