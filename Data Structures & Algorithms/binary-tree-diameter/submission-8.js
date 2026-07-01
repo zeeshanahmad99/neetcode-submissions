df6470ed240308edd321b67908340130b0a8237a
Zeeshan Ahmad <zeems1234@gmail.com>
2026-07-01 22:37:46 +0500

    Add: binary-tree-diameter - submission-8

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-8.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    dfs(root, res) {
+        if(!root) return 0
+
+        const left = this.dfs(root.left, res)
+        const right = this.dfs(root.right, res)
+
+        const sum = left + right
+        res[0] = Math.max(sum, res[0])
+
+        return Math.max(left, right) + 1
+    }
+
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    diameterOfBinaryTree(root) {
+        const res = [0]
+        this.dfs(root, res)
+        return res[0]
+    }
+}
