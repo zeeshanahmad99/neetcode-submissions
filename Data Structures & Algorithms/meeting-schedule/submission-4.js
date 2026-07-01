07b884a6418d4ba480e0d62b5d143d43c7268160
Zeeshan Ahmad <zeems1234@gmail.com>
2026-07-01 23:45:46 +0500

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.js
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        for(let i=0; i<intervals.length; i++) {
+            const {start: start1, end: end1} = intervals[i]
+
+            for(let j=i+1; j<intervals.length; j++) {
+                const {start: start2, end: end2} = intervals[j]
+
+                if(!(start2 >= end1 || end2 < start1)) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+}
