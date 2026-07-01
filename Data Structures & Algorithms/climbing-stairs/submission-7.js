eeda20ac368b9ace09e05c00aedbfd3bfb23e1e9
Zeeshan Ahmad <zeems1234@gmail.com>
2026-07-01 16:24:52 +0500

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.js
@@ -0,0 +1,25 @@
+class Solution {
+    dfs(current, total, cache) {
+        if(current === total) return 1
+        if(current > total) return 0
+        if(cache[current]) return cache[current]
+
+        const single = this.dfs(current + 1, total, cache)
+        const twice = this.dfs(current + 2, total, cache)
+
+        const sum = single + twice
+
+        cache[current] = sum
+
+        return sum
+    }
+
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        return this.dfs(0, n, {})
+    }
+}
+
