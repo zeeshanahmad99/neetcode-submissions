2be3c890ed08f66a12b81a483b6630352e293d7a
Zeeshan Ahmad <zeems1234@gmail.com>
2026-09-02 23:12:38 +0500

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false
+
+        const count = new Array(26).fill(0)
+
+        for(let i=0; i<s.length; i++) {
+            count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++
+            count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--
+        }
+
+        return count.every(val => val === 0)
+    }
+}
