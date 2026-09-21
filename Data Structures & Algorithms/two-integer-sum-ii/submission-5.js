5680918fd2a6f2710da04c603b7f654621960940
Zeeshan Ahmad <zeems1234@gmail.com>
2026-09-21 22:07:37 +0500

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        for(let i=0; i<numbers.length; i++) {
+            const diff = target - numbers[i]
+
+            let left = i + 1
+            let right = numbers.length - 1
+
+            while(left <= right) {
+                const mid = Math.floor((left + right) / 2)
+                const midVal = numbers[mid]
+
+                if(midVal === diff) {
+                    return [i+1, mid+1]
+                } else if(diff < midVal) {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            }
+        }
+    }
+}
