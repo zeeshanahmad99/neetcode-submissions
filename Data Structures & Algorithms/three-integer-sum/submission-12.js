622dabea30741ce8f44194568f9d8ad4e5301e32
Zeeshan Ahmad <zeems1234@gmail.com>
2026-09-22 22:18:46 +0500

    Add: three-integer-sum - submission-12

---
+++ Data Structures & Algorithms/three-integer-sum/submission-12.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        // Time: O(n2)
+        // Space: O(1)
+
+        nums.sort((a, b) => a - b);
+        const cache = new Set();
+
+        const n = nums.length;
+        const result = [];
+
+        for (let i = 0; i < n - 2; i++) {
+            let left = i + 1;
+            let right = n - 1;
+
+            while (left < right) {
+                const sum = nums[left] + nums[right] + nums[i];
+
+                if (sum === 0) {
+                    const key = [nums[i], nums[left], nums[right]].sort((a, b) => a - b).join(",");
+                    if (!cache.has(key)) {
+                        cache.add(key);
+                        result.push([nums[i], nums[left], nums[right]]);
+                    }
+                    right--
+                    left++
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+// -4 -1 -1 0 1 2
